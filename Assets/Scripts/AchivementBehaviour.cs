4214ec2f61e60974d6cc03e31027c924c30ef313
Fabricio <fabricio-gf@hotmail.com>
2018-05-06 18:23:44 -0300

    Added achievement

---
+++ Assets/Scripts/AchivementBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchivementBehaviour : MonoBehaviour {
+
+    Animator animator;
+
+    public float openDuration;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        animator.SetBool("isOpen", true);
+        print("go");
+        StartCoroutine(WaitForClose(openDuration));
+    }
+	
+    IEnumerator WaitForClose(float dur)
+    {
+        print("cr");
+        yield return new WaitForSeconds(dur);
+        print("cr end");
+        animator.SetBool("isOpen", false);
+    }
+}
