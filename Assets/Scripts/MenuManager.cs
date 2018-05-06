6646417725f62215852ad3b8e721fdb0f14b1b8a
Fabricio <fabricio-gf@hotmail.com>
2018-05-05 21:51:11 -0300

    Added UI elements in game scene

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,8 +6,40 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour {
 
 	public AudioSource music;
+    public GameObject quitPanel;
+    bool isQuitPanelOpen = false;
 
-	public void Mute(bool mute){
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            print("pause");
+
+            if (!isQuitPanelOpen)
+                OpenPanel(quitPanel);
+            else
+                ClosePanel(quitPanel);
+        }
+    }
+
+    public void OpenPanel(GameObject panel)
+    {
+        isQuitPanelOpen = true;
+        panel.GetComponent<Animator>().SetBool("isOpen", true);
+    }
+
+    public void ClosePanel(GameObject panel)
+    {
+        isQuitPanelOpen = false;
+        panel.GetComponent<Animator>().SetBool("isOpen", false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Mute(bool mute){
 		music.mute = mute;
 	}
 
